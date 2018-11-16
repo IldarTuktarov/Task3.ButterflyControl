114e74de76b7e8c306b0d9c4eaac4a20097eeca1
Ildar Tuktarov <45071411+IldarTuktarov@users.noreply.github.com>
2018-11-17 00:08:49 +0300

    Delete switchOptions.js

--- js/switchOptions.js
+++
@@ -1,4 +0,0 @@
-allToSelBtn.onclick = function () { allToSelOrAv(firstSelect, secondSelect); }
-allToAvBtn.addEventListener('click', function () { allToSelOrAv(secondSelect, firstSelect) });
-toSelBtn.addEventListener('click', function () { toSelOrAv(firstSelect, secondSelect); });
-toAvBtn.addEventListener('click', function () { toSelOrAv(secondSelect, firstSelect); });
